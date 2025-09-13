4de404b9d69e1fc59613690edcce504932bb8f0e
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-09-13 13:57:05 +0300

    START

---
+++ redesign/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace redesign
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
